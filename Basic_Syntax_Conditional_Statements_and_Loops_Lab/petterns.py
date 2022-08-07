f9f8aaa8be6b68005d48e4b4a19845a1ca452ded
Mark Delchev <markdelchev@gmail.com>
2022-08-07 20:59:10 +0300

    Delete Basic_Syntax_Conditional_Statements_and_Loops_Lab directory

--- Basic_Syntax_Conditional_Statements_and_Loops_Lab/petterns.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-for i in range(n):
-    for j in range(i):
-        print('*', end=" ")
-    print()
-for i in range(n):
-    for j in range(i, n):
-        print('*', end=" ")
-    print()
--- Basic_Syntax_Conditional_Statements_and_Loops_Lab/word_reverse.py
+++
@@ -1,6 +0,0 @@
-word = input()
-length = len(word) - 1
-reversed_word = ""
-for i in range(length, -1, -1):
-    reversed_word += word[i]
-print(reversed_word)
