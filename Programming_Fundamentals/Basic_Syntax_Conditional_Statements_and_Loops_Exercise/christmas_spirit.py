ad9a2a6a2ec4fc0a2fa582d237629837b9508deb
Mark Delchev <markdelchev@gmail.com>
2022-09-18 23:16:12 +0300

    christmas_spirit.py

--- Programming_Fundamentals/Basic_Syntax_Conditional_Statements_and_Loops_Exercise/christmas_spirit.py
+++ Programming_Fundamentals/Basic_Syntax_Conditional_Statements_and_Loops_Exercise/christmas_spirit.py
@@ -8,7 +8,7 @@ total_price = 0
 total_spirit = 0
 for day in range(1, days + 1):
     if day % 11 == 0:
-        quantity *= 2
+        quantity += 2
     if day % 2 == 0:
         total_price += ornament_set[0] * quantity
         total_spirit += ornament_set[1]
@@ -19,10 +19,10 @@ for day in range(1, days + 1):
         total_price += tree_lights[0] * quantity
         total_spirit += tree_lights[1]
     if day % 5 == 0 and day % 3 == 0:
-        total_spirit += 30 + tree_garland[1] + tree_lights[1] 
+        total_spirit += 30 
     if day % 10 == 0:
         total_spirit -= 20
-        total_price += tree_garland[0] + tree_skirt[0] + tree_lights[0] * quantity
+        total_price += tree_garland[0] + tree_skirt[0] + tree_lights[0]
     if day % 10 == 0 and day == days:
         total_spirit -= 30
 print(f"Total cost: {total_price}")
