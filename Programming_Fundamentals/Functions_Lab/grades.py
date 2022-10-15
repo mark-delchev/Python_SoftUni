9671f2620d8ac7828a4afde3e5fa1084c49fe0fa
Mark Delchev <markdelchev@gmail.com>
2022-10-15 18:37:53 +0300

    grades.py

--- Programming_Fundamentals/Functions_Lab/grades.py
+++ Programming_Fundamentals/Functions_Lab/grades.py
@@ -2,7 +2,7 @@ def grades(grade):
     if grade >= 5.50:
         print("Excellent")
     elif 4.50 <= grade < 5.50:
-        print("Very good")
+        print("Very Good")
     elif 3.50 <= grade < 4.50:
         print("Good")
     elif 3.00 <= grade < 3.50:
