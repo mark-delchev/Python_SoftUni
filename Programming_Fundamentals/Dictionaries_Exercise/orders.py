53391bff01c262848e2dd442a83af4b974b14f42
Mark Delchev <markdelchev@gmail.com>
2022-12-11 20:56:25 +0200

    orders.py

--- Programming_Fundamentals/Dictionaries_Exercise/orders.py
+++ Programming_Fundamentals/Dictionaries_Exercise/orders.py
@@ -1,4 +1,7 @@
+
 products = {}
+products_last_price = {}
+products_last_quantity = {}
 while True:
     info = input().split(" ")
     name = info[0]
@@ -7,7 +10,18 @@ while True:
     else:
         price = info[1]
         quantity = info[2]
-        final_price = float(price) * float(quantity)
+        if products.get(name, float(price)) != float(price):
+            final_price = (float(price) * (float(quantity) + products_last_quantity.get(name, 0))) \
+                          - products.get(name, 0)
+        else:
+            final_price = float(price) * float(quantity)
+
+        products_last_price[name] = float(price)
+        if name in products_last_quantity.keys():
+            products_last_quantity[name] += float(quantity)
+        else:
+            products_last_quantity[name] = float(quantity)
+
         if name not in products.keys():
             products[name] = final_price
         else:
